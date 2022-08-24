0a51d398642f4931e5cfaeb59c0cdf72171cee99
shahult <shahult@ibm.com>
2022-08-24 11:53:25 +0200

    k8s.io/klog v1.0.0

---
+++ gloga.go
@@ -0,0 +1,7 @@
+package gloga
+
+import "k8s.io/klog"
+
+func LogA() {
+	klog.Info("Starting transaction...AAA")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/shahulsonhal/gloga
+
+go 1.19
+
+require k8s.io/klog v0.1.0
